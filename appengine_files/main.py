e974bf4537a9a0e1af4e2220be0b4a64a1e1e496
Kishore Narendran <karthikbalakrishnan@Emraos-MacBook-Pro.local>
2013-10-20 01:18:31 +0530

    Data Store
    Data store has been implemented. For accepting new procedures from app
    users.

--- appengine_files/main.py
+++ appengine_files/main.py
@@ -16,11 +16,9 @@
 #
 import webapp2
 import os
+from google.appengine.ext import db
 
 curdir = os.getcwd()
-
-
-
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -65,8 +63,33 @@ class VersionNumber(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(1)
 
+class Procedure(db.Model):
+	query=db.TextProperty()
+	office=db.TextProperty()
+	club=db.TextProperty()
+	fresher=db.TextProperty()
+	p1=db.TextProperty()
+	p2=db.TextProperty()
+	p3=db.TextProperty()
+	p4=db.TextProperty()
+	p5=db.TextProperty()
+	created=db.DateTimeProperty(auto_now_add=True)
 
-
+class SubmitProc(webapp2.RequestHandler):	
+	def get(self,query,office,club,fresher,p1,p2,p3,p4,p5):
+		proc1=Procedure()
+		proc1.query=query
+		proc1.office=office
+		proc1.club=club
+		proc1.fresher=fresher
+		proc1.p1=p1
+		proc1.p2=p2
+		proc1.p3=p3
+		proc1.p4=p4
+		proc1.p5=p5
+		proc1.put()
+		self.response.out.write('<h1>Procedure submitted succesfully</h1>')
+	
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/home', HomeHandler),
@@ -74,6 +97,7 @@ app = webapp2.WSGIApplication([
     ('/officeData', Office_Data),
     ('/procData', Proc_Data),
     ('/Version', VersionNumber),
+	('/submitproc/(.*)/(.*)/(.*)/(.*)/(.*)/(.*)/(.*)/(.*)/(.*)', SubmitProc)
 ], debug=True)
 
 
